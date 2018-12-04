3ba4f2c2cf7b67092d840b4dc91b6171865a7c6c
Sam Murray <samueljmurray@gmail.com>
2018-12-04 17:49:42 +0000

    bump version to 0.1.1 for release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PasswordlessAuth.Mixfile do
   def project do
     [
       app: :passwordless_auth,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
